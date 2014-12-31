c105e05cf409cb37e9772decad7228977384e731
Michele Orsi <morsi@map2app.com>
2014-12-31 16:30:46 +0100

    added fizz buzz problem

---
+++ src/programmingcodeinterview/fizzbuzz/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package programmingcodeinterview.fizzbuzz;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
+        try {
+         line = br.readLine();   
+        } catch(IOException ex) {}
+        int N = Integer.parseInt(line);
+        for(int i=1; i<=N; i++) {
+		  String toPrint = "";
+		  if(i%3==0) {
+			toPrint+="Fizz";
+		  }
+		  if(i%5==0) {
+			toPrint+="Buzz";
+		  }
+		  if(toPrint.isEmpty()) {
+			System.out.println(i);
+		  } else {
+			System.out.println(toPrint);
+		  }
+        } 
+    }
+}
