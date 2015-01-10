0c7293c8f2744931dda9d66c4cc46cb0b7b7961f
Michele Orsi <morsi@map2app.com>
2015-01-10 23:45:47 +0100

    added ddn job interview questio

---
+++ src/ddninterview/Solution.java
@@ -0,0 +1,33 @@
+package ddninterview;
+
+import org.junit.Test;
+
+public class Solution {
+	
+	@Test
+	public void ex1() {
+		String s = " Hello ";
+		s += "World ";
+		s.replace('H', 'M');
+		s.trim();
+		System.out.println(s);
+	}
+	
+	@Test
+	public void ex2() {
+		Object s1 = new String("Hello");
+		Object s2 = new String("Hello");
+
+		System.out.println("ex1:");
+		System.out.println("identity:: " + (s1 == s2) );
+		System.out.println("equality:: " + s1.equals(s2) );
+		
+		s1 = "Hello";
+		s2 = "Hello";
+
+		System.out.println("ex2:");
+		System.out.println("identity:: " + (s1 == s2) );
+		System.out.println("equality:: " + s1.equals(s2) );
+	}
+
+}
