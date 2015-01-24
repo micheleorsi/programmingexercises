f63552b318287d37f824410b59e4325755b3db90
Michele Orsi <morsi@map2app.com>
2015-01-24 18:02:16 +0100

    added all the code did during job interviews

--- src/realjobinterview/SolutionEvaluateString.java
+++ src/realjobinterview/SolutionEvaluateString.java
@@ -18,7 +18,7 @@ import org.junit.Test;
  * @author micheleorsi
  *
  */
-public class Solution {
+public class SolutionEvaluateString {
 
 	/**
 	 * @param args
@@ -26,17 +26,17 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         String inputString = sc.nextLine();
-        System.out.println(Solution.evaluate(inputString));
+        System.out.println(SolutionEvaluateString.evaluate(inputString));
 	}
 	
 	public static int evaluate(String string) {
         String[] linearEvaluation = string.split("-|\\+");
         
-        int result=Solution.doOperations(linearEvaluation[0]);
+        int result=SolutionEvaluateString.doOperations(linearEvaluation[0]);
         int totalLenght=linearEvaluation[0].length();
         for(int i=1; i<linearEvaluation.length; i++) {
-        	int tempResult = Solution.doOperations(linearEvaluation[i]);
-        	result = Solution.doOperations(""+result+string.charAt(totalLenght)+tempResult);
+        	int tempResult = SolutionEvaluateString.doOperations(linearEvaluation[i]);
+        	result = SolutionEvaluateString.doOperations(""+result+string.charAt(totalLenght)+tempResult);
         	totalLenght+=linearEvaluation[i].length()+1;
         }
 		return result;
@@ -71,31 +71,31 @@ public class Solution {
         else {
         	String firstOperation = numbers[0]+realOperators.get(0)+numbers[1];
         	String restOfTheString = string.substring(firstOperation.length(),string.length());
-        	return Solution.doOperations(
-        			Solution.doOperations(firstOperation)+
+        	return SolutionEvaluateString.doOperations(
+        			SolutionEvaluateString.doOperations(firstOperation)+
         			restOfTheString);        	
         }		
 	}
 	
 	@Test
 	public void test1() {
-		assertEquals(6, Solution.doOperations("6"));
-		assertEquals(2, Solution.doOperations("2"));
-		assertEquals(200, Solution.doOperations("200"));
-		assertEquals(6, Solution.doOperations("4+2"));
-		assertEquals(2, Solution.doOperations("4/2"));
-		assertEquals(10*2, Solution.doOperations("10*2"));
-		assertEquals(200-120, Solution.doOperations("200-120"));
-		assertEquals(40*3/2, Solution.doOperations("40*3/2"));
-		assertEquals(10/5*10*20/10, Solution.doOperations("10/5*10*20/10"));
-		assertEquals(14*2/7*10*20, Solution.doOperations("14*2/7*10*20"));
-		assertEquals(14+2+7-10+20, Solution.doOperations("14+2+7-10+20"));
+		assertEquals(6, SolutionEvaluateString.doOperations("6"));
+		assertEquals(2, SolutionEvaluateString.doOperations("2"));
+		assertEquals(200, SolutionEvaluateString.doOperations("200"));
+		assertEquals(6, SolutionEvaluateString.doOperations("4+2"));
+		assertEquals(2, SolutionEvaluateString.doOperations("4/2"));
+		assertEquals(10*2, SolutionEvaluateString.doOperations("10*2"));
+		assertEquals(200-120, SolutionEvaluateString.doOperations("200-120"));
+		assertEquals(40*3/2, SolutionEvaluateString.doOperations("40*3/2"));
+		assertEquals(10/5*10*20/10, SolutionEvaluateString.doOperations("10/5*10*20/10"));
+		assertEquals(14*2/7*10*20, SolutionEvaluateString.doOperations("14*2/7*10*20"));
+		assertEquals(14+2+7-10+20, SolutionEvaluateString.doOperations("14+2+7-10+20"));
 	}
 	
 	@Test
 	public void test2() {
-		assertEquals(185, Solution.evaluate("10+40*3/2+10*44/4+5"));
-		assertEquals(1375, Solution.evaluate("1100-40*4/2+100*14/4+5"));
+		assertEquals(185, SolutionEvaluateString.evaluate("10+40*3/2+10*44/4+5"));
+		assertEquals(1375, SolutionEvaluateString.evaluate("1100-40*4/2+100*14/4+5"));
 	}
 
 }
