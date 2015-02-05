06cc6b4709eeef7974d79e37f73e5127999f7470
Michele Orsi <morsi@map2app.com>
2015-02-05 19:53:55 +0100

    added text for two elevators problem

---
+++ src/realjobinterview/SolutionTwoElevators.java
@@ -0,0 +1,28 @@
+package realjobinterview;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * elevator:
+ * 12 floors
+ * 2 elevators
+ * INPUT: one button for each floor inside and two (up and down) for every floor outside
+ * software that controls the elevator
+ * 
+ * USER STORIES:
+ * - a person at a specif floor request to go up or down
+ * - a person inside the elevator requests to go at a specific floor
+ * 
+ * @author micheleorsi
+ *
+ */
+public class SolutionTwoElevators {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
