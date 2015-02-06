ef412e4d6b39fea9270337078662f5e2a9459dfd
Michele Orsi <morsi@map2app.com>
2015-02-06 13:32:53 +0100

    implemented sort two arrays

--- src/interviewcake/Ex42_MergeSortedArrays.java
+++ src/interviewcake/Ex42_MergeSortedArrays.java
@@ -2,6 +2,8 @@ package interviewcake;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 /**
@@ -22,24 +24,34 @@ public class Ex42_MergeSortedArrays {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int[] array1 = new int[] {3,4,6,10,11,15};
+		int[] array2 = new int[] {1,5,8,12,14,19};
+		
+		int[] result = new int[] {1,3,4,5,6,8,10,11,12,14,15,19};
+		assertEquals(Arrays.toString(result), 
+				Arrays.toString(this.merge_arrays(array1, array2)));
 	}
-	
+	// O(n1+n2)
 	public int[] merge_arrays(int[] arr1, int[] arr2) {
 		int idx1 = 0;
 		int idx2 = 0;
 		int[] result = new int[arr1.length + arr2.length];
 		for(int i=0; i<result.length; i++) {
-			if(idx1<arr1.length && arr1[idx1]<=arr2[idx2]) {
-				result[i]=arr1[idx1];
-				idx1++;
-			} else if(idx2<arr2.length && arr1[idx1]>arr2[idx2]) {
+			if(idx1<arr1.length && idx2<arr2.length) {
+				if(arr1[idx1]<=arr2[idx2]) {
+					result[i]=arr1[idx1];
+					idx1++;
+				} else {
+					result[i]=arr2[idx2];
+					idx2++;
+				}
+			} else if(idx1>=arr1.length) {
 				result[i]=arr2[idx2];
 				idx2++;
-			} else {
+			} else if(idx2>=arr2.length) {
 				result[i]=arr1[idx1];
 				idx1++;
-			}
+			} 
 		}
 		return result;
 	}
