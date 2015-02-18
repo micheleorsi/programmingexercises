dea76bc382f7258dac564b2d2e0802fb596c1d46
Michele Orsi <morsi@map2app.com>
2015-02-18 01:11:04 +0100

    added recursive linked list inversion

--- src/lucainterviews/SolutionInvertList.java
+++ src/lucainterviews/SolutionInvertList.java
@@ -3,8 +3,6 @@
  */
 package lucainterviews;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 /**
@@ -18,7 +16,7 @@ import org.junit.Test;
 public class SolutionInvertList {
 	
 	@Test
-	public void test() {
+	public void testIterative() {
 		Node node6 = new Node(6,null); 
 		Node node5 = new Node(5,node6); 
 		Node node4 = new Node(4,node5); 
@@ -30,22 +28,20 @@ public class SolutionInvertList {
 		
 		Node actualNode = node0;
 		while(actualNode!=null) {
-//			System.out.println(actualNode.value);
 			actualNode = actualNode.next;
 		}
 		
-		this.invertList(node0);
-		
+		this.invertListIterative(node0);
+		System.out.println("iterative");
 		actualNode = node6;
 		while(actualNode!=null) {
 			System.out.println(actualNode.value);
 			actualNode = actualNode.next;
 		}
-		
 	}
 	
 	// iterative
-	public void invertList(Node a) {
+	public void invertListIterative(Node a) {
 		// check edge-case (a null)
 
 		Node previous = null;
@@ -60,7 +56,47 @@ public class SolutionInvertList {
 		}
 
 	}
+	
+	@Test
+	public void testRecursive() {
+		Node node6 = new Node(6,null); 
+		Node node5 = new Node(5,node6); 
+		Node node4 = new Node(4,node5); 
+		Node node3 = new Node(3,node4);  
+		Node node2 = new Node(2,node3); 
+		Node node1 = new Node(1,node2); 
+		Node node0 = new Node(0,node1); 
+		
+		
+		Node actualNode = node0;
+		while(actualNode!=null) {
+			actualNode = actualNode.next;
+		}
+		
+		this.invertListRecursive(node0);
+		System.out.println();
+		System.out.println("recursive");
+		actualNode = node6;
+		while(actualNode!=null) {
+			System.out.println(actualNode.value);
+			actualNode = actualNode.next;
+		}
+	}
+	
+	public void invertListRecursive(Node a) {
+		invert(a.next, a);
+		a.next=null;
+	}
 
+	public void invert(Node actual, Node previous) {
+		if(actual.next==null) {
+			actual.next=previous;
+			return;
+		} else {
+			invert(actual.next, actual);
+			actual.next = previous;
+		}
+	}
 }
 
 class Node {
@@ -71,6 +107,5 @@ class Node {
 		this.value = value;
 		this.next = next;
 	}
-	
 }
 
