22cf5eacd8d2a9c321caf194e7c7a710dddd7cbe
Michele Orsi <morsi@map2app.com>
2015-04-20 15:40:30 +0200

    added another interview question

---
+++ src/realjobinterview/AverageCalculator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package realjobinterview;
+
+/**
+ * let A be the input array of floating point numbers, size N 
+ * let B be the output array, floating point numbers, size N too (this is important) 
+ * let W be a (positive) integer 
+ * Write the algorithm to compute the sliding (moving) average of the elements of A into output B: 
+ * The general formula: B[i] = sum_{k=0..W-1} A[i+k] / W
+ * 
+ * A = [ 3 6 3 1 5 7 8 ] W=3 
+ * B[0] = ( 3 + 6 + 3 ) / 3 
+ * B[1] = ( 6 + 3 + 1 ) / 3
+ * B[2] = (3+1+5)/3 
+ * 
+ * @author micheleorsi
+ *
+ */
+public class AverageCalculator {
+
+}
