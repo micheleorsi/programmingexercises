137bb24595084d78016adda318f79fa039557f66
micheleorsi <morsi@map2app.com>
2015-06-15 21:21:47 +0200

    added text for exercise 5

---
+++ src/interviewcake/Ex05_Coin.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package interviewcake;
+
+/**
+ * https://www.interviewcake.com/question/coin
+ * 
+ * Imagine you landed a new job as a cashier...
+ * Your quirky boss found out that you're a programmer and has a weird request about something they've been wondering for a long time.
+ * Write a function that, given:
+ * 
+ * 1. an amount of money
+ * 2. a list of coin denominations
+ * 
+ * computes the number of ways to make amount of money with coins of the available denominations.
+ * 
+ * Example: for amount=4 (4C) and denominations=[1,2,3](1C, 2C and 3C), your program would output 
+ * 4—the number of ways to make 4C with those denominations
+ * 
+ * 1. 1C, 1C, 1C, 1C
+ * 2. 1C, 1C, 2C
+ * 3. 1C, 3C
+ * 4. 2C, 2C
+ * 
+ * @author morsi
+ *
+ */
+public class Ex05_Coin {
+
+}
