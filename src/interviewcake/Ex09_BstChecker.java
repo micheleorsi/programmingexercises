5d554a64edbfb078bdfe6f34485007ac481fc118
micheleorsi <morsi@map2app.com>
2015-06-15 21:32:44 +0200

    added text for exercise number 9

---
+++ src/interviewcake/Ex09_BstChecker.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package interviewcake;
+
+/**
+ * Write a function to check that a binary tree is a valid binary search tree.
+ * 
+ * @author morsi
+ *
+ */
+public class Ex09_BstChecker {
+
+}
