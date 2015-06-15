b0f47bc9b6780cee328f30f5098feeabc47fec6e
micheleorsi <morsi@map2app.com>
2015-06-15 21:29:16 +0200

    added text for exercise number 8

---
+++ src/interviewcake/Ex08_BalancedBinaryTree.java
@@ -0,0 +1,15 @@
+package interviewcake;
+
+
+/**
+ * 
+ * Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).
+ * 
+ * A tree is "superbalanced" if the difference between the depths of any two leaf nodes is no greater than one.
+ * 
+ * @author morsi
+ *
+ */
+public class Ex08_BalancedBinaryTree {
+
+}
