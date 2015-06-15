249de6e0c1d704809423f18a4eb59ff7042a4b4c
micheleorsi <morsi@map2app.com>
2015-06-15 21:27:50 +0200

    added text for exercise number 7

---
+++ src/interviewcake/Ex07_TemperatureTracker.java
@@ -0,0 +1,29 @@
+package interviewcake;
+
+/**
+ * 
+ * You decide to test if your oddly-mathematical heating company is fulfilling its All-Time Max, Min, Mean and Mode Temperature Guarantee™.
+ * 
+ * Write a class TempTracker with these methods:
+ * 
+ * 1. insert()—records a new temperature
+ * 2. get_max()—returns the highest temp we've seen so far
+ * 3. get_min()—returns the lowest temp we've seen so far
+ * 4. get_mean()—returns the mean ↴ of all temps we've seen so far
+ * 5. get_mode()—returns the mode ↴ of all temps we've seen so far
+ * 
+ * Optimize for space and time. Favor speeding up the get_ functions over speeding up the insert() function.
+ * 
+ * get_mean() should return a float, but the rest of the get_ functions can return integers. 
+ * Temperatures will all be inserted as integers. We'll record our temperatures in Fahrenheit, 
+ * so we can assume they'll all be in the range 0...110
+ * 
+ * If there is more than one mode, return any of the modes.
+ * 
+ * 
+ * @author morsi
+ *
+ */
+public class Ex07_TemperatureTracker {
+
+}
