6153df670acdcbc17d9b52e9f07f20302900e304
micheleorsi <morsi@map2app.com>
2015-06-15 21:25:41 +0200

    added text for exercise number 6

---
+++ src/interviewcake/Ex06_RectangularLove.java
@@ -0,0 +1,34 @@
+package interviewcake;
+
+
+/**
+ * 
+ * A crack team of love scientists from OkEros (a hot new dating site) have devised a way to represent dating profiles as rectangles on a two-dimensional plane.
+ * They need help writing an algorithm to find the intersection of two users' love rectangles. 
+ * They suspect finding that intersection is the key to a matching algorithm so powerful it will cause an immediate acquisition by Google or Facebook or Obama or something.
+ * 
+ * Write a function to find the rectangular intersection of two given love rectangles.
+ * 
+ * As with the example above, love rectangles are always "straight" and never "diagonal." More rigorously: each side is parallel with either the x-axis or the y-axis.
+ * 
+ * They are defined as hash maps ↴ like this:
+ * my_rectangle = { 
+ * 
+ * # coordinates of bottom-left corner:
+ *  'x': 1,
+ *  'y': 5,
+ * 
+ * # width and height
+ * 	'width': 10,
+ *	'height': 4,
+ * }
+ * 
+ * Your output rectangle should use this format as well.
+ * 
+ * 
+ * @author morsi
+ *
+ */
+public class Ex06_RectangularLove {
+
+}
