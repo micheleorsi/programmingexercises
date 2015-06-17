3377bd86041b1682cfb4d7e0fe161354ac08a7fa
micheleorsi <morsi@map2app.com>
2015-06-17 14:45:32 +0200

    added text for exercise number 12

---
+++ src/interviewcake/Ex12_FindInOrderedSet.java
@@ -0,0 +1,13 @@
+package interviewcake;
+
+/**
+ * https://www.interviewcake.com/question/find-in-ordered-set
+ * 
+ * Suppose we had an array of n integers in sorted order. How quickly could we check if a given integer is in the array?
+ * 
+ * @author morsi
+ *
+ */
+public class Ex12_FindInOrderedSet {
+
+}
